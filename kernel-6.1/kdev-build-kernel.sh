0527be20913b950520f9c393ae21503ab93f49c0
yifengyou <842056007@qq.com>
2026-09-11 12:10:51 +0800

    update workflow
    Signed-off-by: yifengyou <842056007@qq.com>

--- kernel-6.1/kdev-build-kernel.sh
+++ kernel-6.1/kdev-build-kernel.sh
@@ -2,8 +2,7 @@
 
 set -x
 
-cd kernel-6.1
-#make clean
+make mrproper
 make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 kedge2_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 rk3588s-khadas-edge2.img -j`nproc`
 
