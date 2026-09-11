d37c8223ba1826ab192c66893afc037edb70b81a
yifengyou <842056007@qq.com>
2026-09-11 12:40:40 +0800

    add bdy g98 kernel-6.1 dts
    Signed-off-by: yifengyou <842056007@qq.com>

--- kernel-6.1/kdev-build-kernel.sh
+++ kernel-6.1/kdev-build-kernel.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -x
+set -xe
 
-make mrproper
+#make mrproper
 make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 kedge2_defconfig
-make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 rk3588s-khadas-edge2.img -j`nproc`
+make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 dtbs
+#make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 rk3588s-khadas-edge2.img -j`nproc`
+make CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 ARCH=arm64 rk3588-bdy-g98.img -j`nproc`
+
 
 
 
