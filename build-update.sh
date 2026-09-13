98edc08e6d1946565c84d65f2792aa359cdd4cd9
yifengyou <842056007@qq.com>
2026-09-13 23:16:57 +0800

    add android update image generate script
    Signed-off-by: yifengyou <842056007@qq.com>

--- build-update.sh
+++ build-update.sh
@@ -1,12 +1,57 @@
 #!/bin/bash
-#
 
+set -x
+WORKDIR=`pwd`
+PACK_TOOL_DIR="${WORKDIR}/RKTools/linux/Linux_Pack_Firmware/"
+OUTPUT_DIR="${WORKDIR}/rockdev/"
 
+# env prep
+mkdir -p ${OUTPUT_DIR}
+chmod +x ${WORKDIR}/RKTools/linux/Linux_Pack_Firmware/rockdev/afptool
+chmod +x ${WORKDIR}/RKTools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker
 
-chmod +x /builder/dc-a588.git/tools/afptool
-chmod +x /builder/dc-a588.git/tools/rkImageMaker
-cd /builder/dc-a588.git/output-updatable-image/RKDevTool/rockdev/image/
-/builder/dc-a588.git/tools/afptool -pack . temp.img
-/builder/dc-a588.git/tools/rkImageMaker -RK3588 MiniLoaderAll.bin temp.img update.img -os_type:androidos
-find . -type f ! -name "update.img" -exec rm -f {} \;
+# gen image
+mkdir -p ${PACK_TOOL_DIR}/rockdev/Image/
+cd ${PACK_TOOL_DIR}/rockdev/Image/
+if [ ! -f MiniLoaderAll.bin ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/MiniLoaderAll.bin
+fi
+if [ ! -f uboot.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/uboot.img
+fi
+if [ ! -f misc.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/misc.img
+fi
+if [ ! -f dtbo.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/dtbo.img
+fi
+if [ ! -f vbmeta.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/vbmeta.img
+fi
+if [ ! -f boot.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/boot.img
+fi
+if [ ! -f recovery.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/recovery.img
+fi
+if [ ! -f baseparameter.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/baseparameter.img
+fi
+if [ ! -f super.img ] ; then
+  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/super.img
+fi
+ls -alh *.img *.bin
 
+# pack image
+echo "Make update.img"
+cd ${PACK_TOOL_DIR}/rockdev && ./mkupdate.sh rk3588 Image
+
+if [ $? -eq 0 ]; then
+    echo "Make update image ok!"
+else
+    echo "Make update image failed!"
+    exit 1
+fi
+
+mv ${PACK_TOOL_DIR}/rockdev/update.img ${OUTPUT_DIR}
+ls -alh ${OUTPUT_DIR}
