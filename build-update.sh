29ca4ae6328dcc776e2db575d7fd90b304065975
yifengyou <842056007@qq.com>
2026-09-13 23:21:22 +0800

    update workflow
    Signed-off-by: yifengyou <842056007@qq.com>

--- build-update.sh
+++ build-update.sh
@@ -14,31 +14,34 @@ chmod +x ${WORKDIR}/RKTools/linux/Linux_Pack_Firmware/rockdev/rkImageMaker
 mkdir -p ${PACK_TOOL_DIR}/rockdev/Image/
 cd ${PACK_TOOL_DIR}/rockdev/Image/
 if [ ! -f MiniLoaderAll.bin ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/MiniLoaderAll.bin
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/MiniLoaderAll.bin
+fi
+if [ ! -f parameter.txt ] ; then
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/parameter.txt
 fi
 if [ ! -f uboot.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/uboot.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_uboot/uboot.img
 fi
 if [ ! -f misc.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/misc.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/misc.img
 fi
 if [ ! -f dtbo.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/dtbo.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/dtbo.img
 fi
 if [ ! -f vbmeta.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/vbmeta.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/vbmeta.img
 fi
 if [ ! -f boot.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/boot.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_kernel/boot.img
 fi
 if [ ! -f recovery.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/recovery.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/recovery.img
 fi
 if [ ! -f baseparameter.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/baseparameter.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/baseparameter.img
 fi
 if [ ! -f super.img ] ; then
-  wget -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/super.img
+  wget -q -c https://github.com/yifengyou/BDY_G98_RK3588-khadas-android14/releases/download/g98_android14_artifacts/super.img
 fi
 ls -alh *.img *.bin
 
